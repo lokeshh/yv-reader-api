74a957b400664874d34d82bddce05ebece20aecc
lokeshh <lokeshhsharma@gmail.com>
2018-02-01 11:55:31 +0000

    remove bad characters

--- app/controllers/verse_controller.rb
+++ app/controllers/verse_controller.rb
@@ -4,12 +4,15 @@ class VerseController < ApplicationController
     chapter = params[:chapter]
     verse = params[:verse]
     verse = Verse.find_by(location: "#{book},#{chapter},#{verse}")
-    render json: (verse ? beautify(verse.value).to_json : '')
+    render json: (verse ? render_verse(verse.value).to_json : '')
   end
   
   private
   
-  def beautify verse
-    verse.split(/\n/)[-2..-1].join '\n'
+  def render_verse verse
+    verse.split(/\n/).last(2).map do |i| 
+      i.strip.gsub(/&nbsp;/, '')
+        .gsub(/ \/<br>/, '')
+    end
   end
 end
